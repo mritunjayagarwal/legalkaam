1edcf2b2aca30c84d89b05f9279bf6a3da5bf8c0
Mritunjay Agarwal <mritunjayagarwal96@gmail.com>
2022-03-16 13:22:18 +0530

    gg

--- controllers/routes.js
+++ controllers/routes.js
@@ -6,6 +6,7 @@ module.exports = function(User, Category, Type){
 
             router.post('/new/category', this.newCategory);
             router.post('/new/subcat', this.newSubCat);
+            router.post('/create/array', this.newArray);
         },
         indexPage: function(req, res){
             // const newCat = new Category({
@@ -21,7 +22,8 @@ module.exports = function(User, Category, Type){
         },
         admin: async function(req, res){
             var subcats = await Category.find({}).exec();
-            res.render('admin.ejs', {subcats: subcats});
+            var types = await Type.find({}).sort('-created').exec();
+            res.render('admin.ejs', {subcats: subcats, types: types});
         },
         newCategory: function(req, res){
             const newCat = new Category({
@@ -35,40 +37,55 @@ module.exports = function(User, Category, Type){
         },
         newSubCat: async function(req, res){
 
-            console.log(req.body.subcat);
+            console.log(req.body);
 
             Category.find({
                 _id: req.body.subcat
             }, (err, category) => {
                 if(category){
+                    // var json = JSON.stringify(req.body);;
 
-                    const newSub = new Type({
-                        category: req.body.subcat,
-                        name: req.body.subcatname,
-                        desc: req.body.desc
-                    });
+                    // var obj = JSON.parse(json);
+                    // var values = Object.keys(obj).map(function (key) { return obj[key]; });
+                    // values = values.splice(3);
+                    // console.log(values);
+
+                    // const newSub = new Type({
+                    //     category: req.body.subcat,
+                    //     name: req.body.subcatname,
+                    //     desc: req.body.desc,
+                    //     steps: values
+                    // });
         
-                    newSub.save(() => {
-                        console.log("Sub Category Created Successfully");
-                    })
+                    // newSub.save(() => {
+                    //     console.log("Sub Category Created Successfully");
+                    // })
 
-                    Category.updateOne({
-                        _id: req.body.subcat
-                    }, {
-                        $push: {
-                            subcat: {
-                                name: newSub.name,
-                                type: newSub._id
-                            }
-                        }
-                    }, (err) => {
-                        console.log("Success")
-                    })
+                    // Category.updateOne({
+                    //     _id: req.body.subcat
+                    // }, {
+                    //     $push: {
+                    //         subcat: {
+                    //             name: newSub.name,
+                    //             type: newSub._id
+                    //         }
+                    //     }
+                    // }, (err) => {
+                    //     console.log("Success")
+                    // })
                 }else{
                     res.redirect("/admin");
                 }
             })
 
+            res.redirect('/admin')
+        },
+        newArray: function(req, res){
+            var json = JSON.stringify(req.body);;
+
+            var obj = JSON.parse(json);
+            var values = Object.keys(obj).map(function (key) { return obj[key]; });
+            console.log(values);
             res.redirect('/admin')
         }
     }
--- models/type.js
+++ models/type.js
@@ -4,7 +4,13 @@ const Schema = mongoose.Schema
 const typeSchema = new Schema({
     category: { type: Schema.Types.ObjectId, ref: 'Category'},
     name: { type: String, lowercase: true},
-    desc: { type: String}
+    desc: { type: String},
+    steps: [
+        {
+            type: String
+        }
+    ],
+    created: { type: Date, default: Date.now}
 });
 
 module.exports = mongoose.model('Type', typeSchema);
